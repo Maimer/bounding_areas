e4379ce85896f1813645982b7b3157f9aff40267
Maimer <nesnl23@yahoo.com>
2014-06-07 10:43:09 -0400

    Added all methods for both classes.  All tests passing.

--- lib/bounding_area.rb
+++ lib/bounding_area.rb
@@ -1,2 +1,12 @@
 class BoundingArea
+  def initialize(boxes = [])
+    @boxes = boxes
+  end
+
+  def contains_point?(x, y)
+    @boxes.each do |box|
+      if box.contains_point?(x, y) then return true end
+    end
+    false
+  end
 end
--- lib/bounding_box.rb
+++ lib/bounding_box.rb
@@ -1,2 +1,36 @@
 class BoundingBox
+  def initialize(x, y, width, height)
+    @x = x
+    @y = y
+    @width = width
+    @height = height
+  end
+
+  def width
+    @width
+  end
+
+  def height
+    @height
+  end
+
+  def left
+    @x
+  end
+
+  def right
+    @x + @width
+  end
+
+  def top
+    @y + @height
+  end
+
+  def bottom
+    @y
+  end
+
+  def contains_point?(x, y)
+    x >= @x && x <= @x + @width && y >= @y && y <= @y + @height
+  end
 end
